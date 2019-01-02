de295086258d5238e320c212a09806fea047bb16
TheAnilDeveloper <anilsri.04@gmail.com>
2019-01-02 16:21:03 +0530

    Update Program.cs

--- Jan2ConsoleApp1/Program.cs
+++ Jan2ConsoleApp1/Program.cs
@@ -8,8 +8,21 @@ namespace Jan2ConsoleApp1
 {
     class Program
     {
+        static double RaiseToPower(double number, int power)
+        {
+            double result = 1;
+            for (int i = 0; i < power; i++)
+                result *= number;
+
+            return result;
+        }
+
         static void Main(string[] args)
         {
+            double d=RaiseToPower(2, 5);
+            Console.WriteLine("The Answer is   \n");
+            Console.WriteLine(d);
+            Console.ReadLine();
         }
     }
 }
